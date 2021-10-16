bd374c8188fbb4c4fefa6484376f4ce3efcd1c48
Mateusz Grabowski <mategra5@gmail.com>
2021-10-16 16:06:58 +0200

    added MVC classes

--- src/main/java/com/mycompany/cw_1_projekt/WordsStatistics.java
+++ src/main/java/com/mycompany/cw_1_projekt/WordsStatistics.java
@@ -4,15 +4,27 @@
  */
 package com.mycompany.cw_1_projekt;
 
+import java.io.*;
+import java.util.*; 
+
 /**
  *
  * @author Mateusz Grabowski
  */
 public class WordsStatistics {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
-    }
-
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter path to the file: ");  
+        String filePath= sc.nextLine();
+        System.out.println("You have entered: "+filePath);
+        
+        FileReader fr = new FileReader(filePath);
+ 
+        int i;
+        while ((i = fr.read()) != -1)
+            System.out.print((char)i);
     
+    }    
 }
