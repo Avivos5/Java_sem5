a5abc75052072afae220eccebc85c2900edc38b7
Mateusz Grabowski <mategra5@gmail.com>
2021-11-20 18:18:00 +0100

    name refactor

--- src/main/java/com/mycompany/words_statistics/IsEmptyException.java
+++ src/main/java/com/mycompany/words_statistics/IsEmptyException.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.cw_1_projekt;
+package com.mycompany.words_statistics;
 
 /**
  * Exception class for objects thrown when attempting to return empty value.
--- src/main/java/com/mycompany/words_statistics/TextFileStatsModel.java
+++ src/main/java/com/mycompany/words_statistics/TextFileStatsModel.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.cw_1_projekt;
+package com.mycompany.words_statistics;
 
 /**
  * Model class of the text file statistics object.
